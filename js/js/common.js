ced5730e3752a0d8656b931cd64e3b0ff6caac87
AlexGo <gulinskyi72@gmail.com>
2017-02-23 22:03:07 +0200

    init

---
+++ js/js/common.js
@@ -0,0 +1,50 @@
+$(function() {
+
+	
+})
+
+$(document).ready(function(){
+	$('.slider').slick({
+		// autoplay: true,
+		autoplaySpeed: 2000,
+		speed: 2000,
+		arrows: true,
+		centerMode: false,
+		zIndex: 0
+		// centerPadding: '0px',
+		// dots: true
+	});
+});
+
+$(document).ready(function(){
+	$('.block_slide').slick({
+		// autoplay: true,
+		autoplaySpeed: 1000,
+		speed: 3000,
+		arrows: false,
+		// centerMode: true,
+		// centerPadding: '200px',
+		// slidesToShow: 3
+		dots: true
+	});
+});
+
+$(document).ready(function(){
+	$('.block_slide_2').slick({
+		autoplay: true,
+		autoplaySpeed: 1000,
+		speed: 3000,
+		arrows: false,
+		centerMode: false,
+		// centerPadding: '0px',
+		dots: true
+	});
+});
+
+$(document).ready(function() {
+
+	
+	$("menu_button").click(function() {
+		$("#nav ul").slideToggle();
+	
+	});
